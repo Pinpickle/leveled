3f70de86d2176861bb29260740ee7a37aad6d918
BuildTools <christian@decode.org.nz>
2015-07-25 12:40:01 +1200

    Add Layer class

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,22 @@
 var React = require('react');
+var Layer = require('./layer');
 
 var App = React.createClass({
   render: function () {
-    return (<h1>Hello World!</h1>);
+    
+    var layerNodes = this.props.layers.map(function (layer) {
+      return (
+        <Layer />
+      );
+    });
+
+    return (
+      <div>
+        <h1>Hello World!</h1>
+        {layerNodes}
+      </div>
+    );
   }
 });
 
-React.render(<App />, document.body);
+React.render(<App layers={[1, 2, 3]} />, document.body);
---
+++ scripts/layer.js
@@ -0,0 +1,7 @@
+var React = require('react');
+
+var Layer = module.exports = React.createClass({
+  render: function () {
+    return (<div><p>Hi I'm a layer</p></div>);
+  }
+});
