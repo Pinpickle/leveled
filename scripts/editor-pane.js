f8c86e5ebc5896d844cbbb66d2ca546628ed95dc
Christian Silver <christian@decode.org.nz>
2015-08-19 18:06:09 +1200

    Add text editor and tabs to editor pane

--- scripts/editor-pane.js
+++ scripts/editor-pane.js
@@ -1,9 +1,22 @@
 var React = require('react');
+var AceEditor = require('react-ace');
+var brace = require('brace');
+
+require('brace/mode/yaml');
+require('brace/theme/github');
 
 var EditorPane = module.exports = React.createClass({
   render: function () {
     return (
-      <div className="editor-pane">Editor Pane!</div>
+      <div className="editor-pane">
+        <div className="editor-tabs">
+          <div className="editor-tab editor-tab-active">Level</div>
+          <div className="editor-tab">Global</div>
+        </div>
+        <div className="editor-content">
+          <AceEditor mode="yaml" theme="github" width="auto" height="auto" />
+        </div>
+      </div>
     )
   }
 });
