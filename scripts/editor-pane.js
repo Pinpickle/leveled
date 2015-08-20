e7e60a70a843c474afd2e0028310c83f3ad6d071
Christian Silver <christian@decode.org.nz>
2015-08-20 17:42:37 +1200

    Add tab changing

--- scripts/editor-pane.js
+++ scripts/editor-pane.js
@@ -1,20 +1,92 @@
 var React = require('react');
 var AceEditor = require('react-ace');
 var brace = require('brace');
+var cx = require('classnames');
 
 require('brace/mode/yaml');
 require('brace/theme/github');
 
+var EditorTabButton = React.createClass({
+  getDefaultProps: function () {
+    return {
+      active: false,
+      name: 'Tab',
+      onClick: function () { }
+    };
+  },
+
+  render: function () {
+    var classes = cx({
+      'editor-tab-active': this.props.active,
+      'editor-tab': true
+    });
+
+    return (
+      <div className={classes} onClick={this.props.onClick}>{this.props.name}</div>
+    );
+  }
+});
+
 var EditorPane = module.exports = React.createClass({
+  getInitialState: function () {
+    return {
+      currentTab: 'level'
+    };
+  },
+
+  setTab: function (tab) {
+    this.setState({
+      currentTab: tab
+    });
+
+    var session = this.aceEditor.sessions[tab];
+    this.aceEditor.editor.setSession(session);
+  },
+
+  componentDidMount: function () {
+    this.aceEditor.editor = brace.edit('editor-text-editor');
+
+    var session = this.aceEditor.sessions[this.state.currentTab];
+    this.aceEditor.editor.setSession(session);
+
+    this.aceEditor.editor.on('change', this.onEditorChange);
+  },
+
+  componentWillMount: function () {
+    this.aceEditor = { };
+
+    this.aceEditor.sessions = {
+      level: brace.createEditSession('', 'ace/mode/yaml'),
+      global: brace.createEditSession('', 'ace/mode/yaml')
+    };
+  },
+
+  componentWillUnmount: function () {
+    this.aceEditor.off('change', this.onEditorChange);
+  },
+
+  onEditorChange: function () {
+    var value = this.aceEditor.editor.getValue();
+    if (this.props.onChange) {
+      this.props.onChange(value);
+    }
+  },
+
   render: function () {
     return (
       <div className="editor-pane">
         <div className="editor-tabs">
-          <div className="editor-tab editor-tab-active">Level</div>
-          <div className="editor-tab">Global</div>
+          <EditorTabButton
+            active={this.state.currentTab == 'level'}
+            name='Level'
+            onClick={this.setTab.bind(this, 'level')}/>
+          <EditorTabButton
+            active={this.state.currentTab == 'global'}
+            name='Global'
+            onClick={this.setTab.bind(this, 'global')} />
         </div>
         <div className="editor-content">
-          <AceEditor mode="yaml" theme="github" width="auto" height="auto" />
+          <div id="editor-text-editor"></div>
         </div>
       </div>
     )
