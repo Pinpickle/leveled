d8aeb1b084c263243f1ba64dcdd06683401b44bf
Christian Silver <christian@decode.org.nz>
2015-08-21 22:06:41 +1200

    Add grid tooltip

--- scripts/preview-pane.js
+++ scripts/preview-pane.js
@@ -2,8 +2,50 @@ var React = require('react');
 var Layer = require('./layer');
 var _ = require('lodash');
 var levelData = require('./level-data');
+var cx = require('classnames');
+var jQuery = require('jquery');
 
 var GridOverlay = React.createClass({
+  getInitialState: function () {
+    return {
+      popup: {
+        active: false,
+        x: 0,
+        y: 0
+      }
+    };
+  },
+
+  componentDidMount: function () {
+    this.DOMNode = React.findDOMNode(this.refs.gridOverlayElement);
+  },
+
+  setPopupPosition: function (e) {
+    var offset = jQuery(this.DOMNode).offset();
+    var top = offset.top;
+    var left = offset.left;
+
+    // console.log(top, left);
+
+    this.setState({
+      popup: {
+        active: true,
+        x: e.pageX - left,
+        y: e.pageY - top
+      }
+    });
+  },
+
+  onMouseLeave: function (e) {
+    this.setState({
+      popup: {
+        active: false,
+        x: this.state.popup.x,
+        y: this.state.popup.y
+      }
+    });
+  },
+
   getDefaultProps: function () {
     return {
       gridSize: 24
@@ -20,8 +62,44 @@ var GridOverlay = React.createClass({
       backgroundImage: 'linear-gradient(0deg, ' + gridline + '), ' +
         'linear-gradient(90deg, ' + gridline + ')'
     }
+
+    var popup = this.state.popup;
+    var gridX = Math.floor(popup.x / this.props.gridSize);
+    var gridY = Math.floor(popup.y / this.props.gridSize);
+
+    var popupClasses = cx({
+      'grid-overlay-popup': true,
+      'grid-overlay-popup-active': popup.active
+    });
+
+    var popupStyle = {
+      top: (gridY + 1) * this.props.gridSize,
+      left: gridX * this.props.gridSize
+    }
+
+    var popupHighlightStyle = {
+      width: this.props.gridSize,
+      height: this.props.gridSize,
+      top: -this.props.gridSize
+    }
+
     return (
-      <div className="grid-overlay" ref="gridOverlayElement" style={style}></div>
+      <div
+        className="grid-overlay"
+        ref="gridOverlayElement"
+        onMouseEnter={this.setPopupPosition}
+        onMouseMove={this.setPopupPosition}
+        onMouseLeave={this.onMouseLeave}
+        style={style}>
+          <div className={popupClasses} style={popupStyle} >
+            <div className="grid-overlay-popup-highlight" style={popupHighlightStyle} ></div>
+            <div className="grid-overlay-popup-content">
+              Cell {gridX}:{gridY} - {gridX * this.props.gridSize},{gridY * this.props.gridSize}<br />
+              Mouse - {popup.x},{popup.y}<br />
+              Center - {(gridX + .5) * this.props.gridSize},{(gridY + .5) * this.props.gridSize}
+            </div>
+          </div>
+      </div>
     );
   }
 });
@@ -42,8 +120,8 @@ var PreviewPane = module.exports = React.createClass({
     return (
       <div className="preview-pane">
         <div className="level-bounds" style={levelStyles}>
-          <GridOverlay gridSize={this.props.globalData.gridSize}/>
           {layerNodes}
+          <GridOverlay gridSize={this.props.globalData.gridSize}/>
         </div>
       </div>
     );
